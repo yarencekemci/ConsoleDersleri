9bafdc8cabe191633bf6445a5e7c5d7a6242a652
yarencekemci <yarencekemci@gmail.com>
2026-01-13 21:29:04 +0300

    odev

---
+++ 261301_Odevler/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _261301_Odevler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan alınan 4 sayı için aşağıdaki işlemlerin sonuçlarını hesaplayıp ekranda gösteriniz.
+            // 1- 4 sayının toplamı
+            // 2- 1. 2. sayıların çarpım sonucuna 5. sayıya bölme
+            // 3- 1. ve 2. sayıların toplam sonucu, 3. sayıdan 4. sayının çıkartılması sonucu bölüm
+            // 4- 4. sayıya diğer 3 sayının toplamına bölünmesi
+            // 5- 1. 2. 3. sayiların çarpımının sonucunun 4 sayıya bölünmesi
+
+        }
+    }
+}
