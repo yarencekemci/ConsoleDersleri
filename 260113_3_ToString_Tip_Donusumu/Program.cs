5d0347888ff50adf4fe472e5f3cf0433323f0561
yarencekemci <yarencekemci@gmail.com>
2026-01-13 20:58:36 +0300

    To string işlendi

---
+++ 260113_3_ToString_Tip_Donusumu/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _260113_3_ToString_Tip_Donusumu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             ToString()
+            herhangi bir tipteki değeri string yapmak için kullanılır
+             */
+
+            int sayi1 = 30;
+            double sayi2 = 90.5;
+            DateTime tarih = DateTime.Now;
+
+            string metin1 = sayi1.ToString();
+            string metin2 = sayi2.ToString();
+            string metin3 = tarih.ToString();
+            Console.WriteLine("Tam sayı string:" + metin1);
+            Console.WriteLine("TOndalık sayı string:" + metin2);
+            Console.WriteLine("Tarih string:" + metin3);
+
+        }
+    }
+}
