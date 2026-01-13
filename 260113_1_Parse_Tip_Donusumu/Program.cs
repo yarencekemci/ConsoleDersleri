c54089863c0002a4146d0ff92eeeedaef14bc4ba
yarencekemci <yarencekemci@gmail.com>
2026-01-13 20:08:26 +0300

    Parse işlendi

---
+++ 260113_1_Parse_Tip_Donusumu/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _260113_1_Parse_Tip_Donusumu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parse projesi");
+            //Convert?? - herhangi bir tipteki veriyi istenilen tipe dönüştürür
+            //Parse- sadece string olan bir tipteki datayı istenilen tipe dönüştürür.
+            //ctrl+k+d kodları hizalar
+            string yaziSayi = "1000";
+            int sayi1 = 250;
+
+            int s1 = Convert.ToInt32(yaziSayi);
+            int s2 = int.Parse(yaziSayi);
+
+            double d1=Convert.ToDouble(sayi1);
+            double d2 = double.Parse(yaziSayi);
+
+            Console.WriteLine("1. sayıyı giriniz");
+            int deger1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("2. sayıyı giriniz");
+            int deger2 = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = deger1 + deger2;
+            Console.WriteLine("Girilen 2 sayının toplamı:" + toplam);
+        }
+    }
+}
