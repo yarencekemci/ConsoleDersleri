6ef08aae2145da9dc14a9e61d831b826a3e41cdc
yarencekemci <yarencekemci@gmail.com>
2026-01-15 20:00:26 +0300

    Aritmetik operatorler

---
+++ 260115_1_Aritmetik_Operatorler/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _260115_1_Aritmetik_Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *Operatörler, programlama dillerinde belirli işlemleri gerçekleştirmek için kullanılan sembollerdir.
+             1- Aritmetik
+             2- Karşılaştırma
+             3- Atama
+             4- Mantıksal
+             */
+            // Aritmetik operatörler: +,-,*,/,% (toplama,çıkartma,çarpma,bölme,mod alma)
+            int sayi1 = 57;
+            int sayi2 = 25;
+            int topla= sayi1 + sayi2;
+            int carp = sayi1 * sayi2;
+            int bol = sayi1 / sayi2;
+            int cikart = sayi1 - sayi2;
+
+            //mod alma- bir sayının diğer sayıya  bölümünden modu verir.
+            int modAl = sayi1 * sayi2;
+            Console.WriteLine("Toplam: "+topla);
+            Console.WriteLine("Cikarma: " + cikart);
+            Console.WriteLine("Bolme: " + bol);
+            Console.WriteLine("Carpma: " + carp);
+            Console.WriteLine("Mod Alma: " + modAl);
+
+        }
+
+    }
+}
