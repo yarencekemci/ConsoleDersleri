2045457998ececa5938131f7d921e1c063830b77
Yaren Çekemci <93402572+yarencekemci@users.noreply.github.com>
2026-01-15 19:29:52 +0300

    Update Program.cs

--- 261301_Odevler/Program.cs
+++ 261301_Odevler/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             //kullanıcıdan alınan 4 sayı için aşağıdaki işlemlerin sonuçlarını hesaplayıp ekranda gösteriniz.
-            // 1- 4 sayının toplamı
+            // 1'den 4'e kadar sayının toplamı
             // 2- 1. 2. sayıların çarpım sonucuna 5. sayıya bölme
             // 3- 1. ve 2. sayıların toplam sonucu, 3. sayıdan 4. sayının çıkartılması sonucu bölüm
             // 4- 4. sayıya diğer 3 sayının toplamına bölünmesi
@@ -30,12 +30,12 @@
             int sayi4 = int.Parse(Console.ReadLine());
 
             //1
-            int islem1 = sayi1 + sayi4;
-            Console.WriteLine("1. sayi ve 4. sayinin toplamı: " + islem1);
+            int islem1 = sayi1 + sayi2 + sayi3+ sayi4;
+            Console.WriteLine("1den 4'e kadar olan sayilarin toplami: " + islem1);
 
             //2
-            double islem2 = (double)(sayi1 * sayi2) / (sayi5);
-            Console.WriteLine("1. ve 2 sayinin carpim sonucunu 5. sayiya bolme" + islem2);
+            double islem2 = (double)(sayi1 * sayi2) / (sayi4);
+            Console.WriteLine("1. ve 2 sayinin carpim sonucunu 4. sayiya bolme" + islem2);
 
             //3
             double islem3 = (double)(sayi1 + sayi2) / (sayi3 - sayi4);
