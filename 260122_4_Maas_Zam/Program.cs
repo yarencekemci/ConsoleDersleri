9ed8a6a9cb74537a93e8e14b0570425d362e96e1
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-22 21:27:12 +0300

    Odev

---
+++ 260122_4_Maas_Zam/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _260122_4_Maas_Zam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             kullanıcıdan alınan bir maas icin
+            eger maas degeri 25.000 ve altında ise %45 zam
+            eger maas degeri 50.000 ve üstünde ise %17 zam
+            eger maas 100.000 ve üstünde ise %9 zam 
+            eger maas 150.000 ve üstünde ise %5 zam yapacak şekilde yazılan zam oranını ve yeni maaş kullaniciya gosteren yapiyi kodlayiniz
+
+             
+             
+             */
+        }
+    }
+}
