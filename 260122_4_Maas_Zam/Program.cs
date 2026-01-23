2fa5760d240a88c474299739106f0f33b72a5078
yarencekemci <yarencekemci@gmail.com>
2026-01-23 10:22:18 +0300

    Odev tamamlandi

--- 260122_4_Maas_Zam/Program.cs
+++ 260122_4_Maas_Zam/Program.cs
@@ -11,9 +11,33 @@
             eger maas 100.000 ve üstünde ise %9 zam 
             eger maas 150.000 ve üstünde ise %5 zam yapacak şekilde yazılan zam oranını ve yeni maaş kullaniciya gosteren yapiyi kodlayiniz
 
-             
-             
-             */
+            */
+            Console.WriteLine("Maasiniz giriniz: ");
+            double maas = Convert.ToDouble(Console.ReadLine());
+            double yeniMaas;
+
+            if(maas>= 150000)
+            {
+                yeniMaas = maas + (maas * 0.05);
+                Console.WriteLine("Yeni zam oraniniz ile maasiniz (0.05): ");
+            }
+            else if (maas >=100000)
+            {
+                yeniMaas = maas + (maas * 0.09);
+                Console.WriteLine("Yeni zam oraniniz ile maasiniz (0.09): ");
+            }
+            else if(maas >= 50000)
+            {
+                yeniMaas = maas + (maas * 0.17);
+                Console.WriteLine("Yeni zam oraniniz ile maasiniz (0.17): ");
+            }
+            else
+            {
+                yeniMaas = maas + (maas * 0.45);
+                Console.WriteLine("Yeni zam oraniniz ile maasiniz (0.45): ");
+            }
+
+            Console.WriteLine("Yeni maasiniz: " + yeniMaas);
         }
     }
 }
