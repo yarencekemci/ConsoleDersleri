7c2efff7d544db54837304289a9253adbc080d04
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-23 19:38:02 +0300

    Switch-Case Tanım

---
+++ 260123_1_Switch_Case/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _260123_1_Switch_Case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             switch case karar yapisi sadece esitlik durumunda calisan bir karar yapisidir
+             */
+            int sayi = 0;
+
+            switch (sayi) // degisken sadece tanımlanır, burada tanımlanan degisken herhangi bir karsilastirma operatörü ile kullanılmaz, sadece case ler icin hangi degisken kullanılacak belirtilir
+            {
+                case 1: // sayi degiskeni 1'e esit oldugunda buradaki kodlar okunacaktir
+                    // kodlaması
+                    break; // 1 icin son
+                case 2:
+                    break; // her bir case icin break ile sonlanir
+                default:
+                    // bir deger almaz, eger case lerde verilen deger sayi degiskeninde yoksa default kısmındaki kodlar okunacaktir: if-else yapisindaki else gibi calisir
+                    //kodlar 
+                    break; // default icin son
+                case 56:
+                    //56 icin kodlar
+                    break;
+
+            
+            }
+
+            string metin = "merhaba";
+
+            switch (metin)
+            {
+                default:
+                    break;
+                    // 1 den fazla case break ile sonlanabilir
+                case "hello":
+                case "merhaba":
+                case "":
+                    break;
+
+                case "slaw":
+                    break;
+                case "hallo":
+                    break;
+
+            }
+        }  
+    }
+}
