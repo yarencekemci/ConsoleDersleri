cb89f347875f9ef4011b6f45f9726b529edd98ae
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-23 21:25:19 +0300

    Odev

---
+++ 260123_6_While_Ornek2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _260123_6_While_Ornek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            1) 1-100 arasindaki sayilari ekrana yazin ve toplamlarini hesaplayiniz
+            2) 55-255 arasindaki 5in katlari ekrana yazin ve toplamlarini hesaplayiniz
+            3) 500 den 0'a doğru 13 katlarini ekranda yaziniz (tersten sayim olacak)
+            4) 1-10000 arasindaki asal sayilarin toplamini hesaplayiniz
+            
+            */
+        }
+    }
+}
