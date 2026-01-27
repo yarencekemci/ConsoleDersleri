5a75f1921a54d6d6812f72bd855330d4a8388e46
yarencekemci <yarencekemci@gmail.com>
2026-01-27 16:18:15 +0300

    While ornek 1

---
+++ 260123_5_While_Ornek1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _260123_5_While_Ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5 ile 135 arasindaki sayilari ekrana yaziniz
+            int sayi = 5;
+            // bu sayilarin toplamlarini hesaplayalim
+            int toplam = 0;
+            while (sayi <= 135)
+            {
+                // toplam = toplam + sayi;
+                toplam += sayi;
+                Console.WriteLine(sayi + ",");
+                sayi++;
+            }
+            Console.WriteLine("5 ve 135 arasindaki sayilarin toplami: " + toplam);
+        }
+    }
+}
