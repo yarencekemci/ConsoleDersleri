d057c1070d456376b8177712680a1d8b654c2ae7
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-27 21:33:47 +0300

    Sorular

---
+++ 260127_3_DoWhile_Ornek1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _260127_3_DoWhile_Ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan alınan 5 sayıdan en büyük sayıyı hesaplayan yapıyı kodlayınız
+           
+            //kullanıcıdan alınan 5 sayıdan en büyük sayıyı hesaplayan yapıyı kodlayınız
+
+            //kullanıcıdan negatif sayı alınakadar girilen pozitif sayıların toplamını ve adetini hesaplayan do while yapısını kodlayınız
+
+            //kullanıcıdan 1-100 arasında sayı girmesini isteyiniz, girilen sayı 1-100 arasında değil ise tekrar sayı girmesini isteyiniz, girilen sayı 1-100 arasında ise sayıyı ekranda gösteriniz ve 1-100 arasında 11 sayı girildikten sonra işlemi sonlandırıp girilen sayıların toplamını ekranda gösteriniz
+
+            //kullanıcı adı admin, şifresi 12?ab olan bir sistem için 3 defa giriş hakkı tanıyan do while sistemini kodlayınız
+
+
+
+        }
+    }
+}
