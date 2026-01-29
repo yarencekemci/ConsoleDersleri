617198b47ff2e63f12a8ab1d72c039d4731dec7d
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-29 21:00:13 +0300

    Ornek

---
+++ 260129_5_For_DıkUcgen/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _260129_5_For_DıkUcgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+            *
+            **
+            ***
+            ****
+            *****
+
+             */
+
+           
+            string metin = "*";
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(metin);
+                metin = metin + "*";
+            }
+            Console.WriteLine("");
+        }
+    }
+}
