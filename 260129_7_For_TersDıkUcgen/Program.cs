d678e16c4b17fb94d4191770c8c4fab080878a57
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-29 21:29:16 +0300

    Ornek yapildi

---
+++ 260129_7_For_TersDıkUcgen/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _260129_7_For_TersDıkUcgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+                        *
+                      ***
+                     ****
+                    *****
+                   ******
+                 ********
+            */
+
+            string yildiz = "*";
+            for (int i = 0; i <= 15; i++)
+            {
+                
+                for (int k = 15; k > i; k--)
+                {
+                    Console.Write(" ");
+                    
+                }
+                Console.WriteLine(yildiz);
+                yildiz += "*";
+
+            }
+        }
+    }
+}
