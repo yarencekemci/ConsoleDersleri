4c298516b7d5fd3e6804808a615ea38064767b00
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-29 21:19:03 +0300

    Yildiz sorusu

---
+++ 260129_6_For_TersSaymaİslemi/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _260129_6_For_TersSaymaİslemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // dik ücgen karakter sayisi max alıncak sekilde max degerden 0 dogru ayni satida yazdiriniz
+
+            string yildiz = "*";
+          
+            for (int i = 1; i < 10; i++)
+            {
+
+                Console.Write(yildiz+"-");
+                
+                for (int k = i; k >= 0; k--)
+                {
+                    Console.Write(k+",");
+                    
+                }
+                Console.WriteLine();
+                yildiz += "*";
+
+            }
+        }
+    }
+}
