a9a0f519952290ddb8b1b7359a4f141c03142680
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-29 20:48:48 +0300

    Ornek

---
+++ 260129_4_For_Dongusu_TersGosterim/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _260129_4_For_Dongusu_TersGosterim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //0-50 arasinda her ekrana yazilacak sayi icin buyukten kucuge ekranda sayilari gosteriniz (0'a kadar)
+
+            for (int i = 0; i <=25; i++)
+            {
+                Console.Write(i+"-");
+                for (int k = i; k >= 0; k--)
+                {
+                    Console.Write(k+",");
+                }
+                Console.WriteLine("");
+            }
+            
+        }
+    }
+}
