1584905cc957461967759d70bfaf327d45d327d7
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-30 21:16:17 +0300

    Diziler

---
+++ 260130_5_DiziOrnek/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _260130_5_DiziOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //11 sayi aliniz, girilen 11 sayiyi listelerken cift olan sayilarin sagında cift, tek olan sayilarin saginda tek yaziniz
+            int[] sayilar = new int[11];
+            int elemansayisi = sayilar.Length;
+            
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine(i+1+". eleman: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                sayilar[i] = elemansayisi;
+                
+            }
+            for (int i = 0; i < elemansayisi; i++)
+            {
+                //if (i % 2 == 0)
+                //{
+                //    Console.WriteLine((i) + "cift");
+                //    i++;
+                //}
+                //if (i % 2 != 0)
+                //{
+                //    Console.WriteLine((i) + "tek");
+                //    i++;
+                //}
+                string yaz = sayilar[i] % 2 == 0 ? "-Çift" : "-Tek";
+                Console.WriteLine(sayilar[i]+yaz);
+            }
+        }
+    }
+}
