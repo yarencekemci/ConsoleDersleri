f3a9ddc945a698a7d7d2501e272dea81d39ec672
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-30 20:42:14 +0300

    Ornek

---
+++ 260130_2_DızıOrnek/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _260130_2_DızıOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanicidan alinan 7 sayinin alindiktan sonra ekranda gosteriniz toplamlarini hesaplayiniz
+
+            int[] sayilar = new int[7];
+            int elemanSayisi = sayilar.Count();
+            int toplam = 0;
+
+            for (int i = 0; i < elemanSayisi; i++)
+            {
+                Console.WriteLine((i+1)+". sayiyi giriniz");
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+                toplam += sayilar[i];
+            }
+            for (int i = 0; i < elemanSayisi; i++)
+            {
+                Console.WriteLine(i+1+". eleman: "+sayilar[i]);
+            }
+            Console.WriteLine("Toplamlari: " + toplam);
+        }
+    }
+}
---
+++ 260130_3_Dizi_While/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _260130_3_Dizi_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanicidan alinan 7 sayiyi aldiktan sonra ekranda gosterip toplamlarini hesaplayiniz
+
+            int[] sayilar = new int[7];
+            int elemanSayisi = sayilar.Count();
+            int toplam = 0;
+            int i = 0;
+            do
+            {
+                
+                Console.WriteLine((i+1) + ". sayiyi giriniz.");
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+                toplam += i;
+                i++;
+            } while (i < elemanSayisi);
+            
+            
+            Console.WriteLine("toplam: " + toplam);
+            Console.WriteLine("---------son");
+             Console.ReadLine();
+            
+        }
+    }
+}
