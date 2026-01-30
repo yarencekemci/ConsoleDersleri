d5d1873a8f27459e08dd86f264ea425c46eb8ce4
Yaren Çekemci <yarencekemci@gmail.com>
2026-01-30 21:27:14 +0300

    Dizi Rastgele

---
+++ 260130_6_Dizi_Random/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _260130_6_Dizi_Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // random - rastgele sayi üretmek icin kullanilan bir class yapisidir
+            Random rastgele = new Random();
+            Console.WriteLine(rastgele.Next(200));
+
+            for (int i = 0; i < 150; i++)
+            {
+                int sayi = rastgele.Next(200);
+                Console.Write(sayi+",");
+            }
+            /*
+           
+
+            */
+
+        }
+    }
+}
