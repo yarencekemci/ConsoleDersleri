de6e1c8a30613305205ecb1fa29b3d1652e30248
Yaren Çekemci <yarencekemci@gmail.com>
2026-02-12 20:52:35 +0300

    Elektrik Faturası - example 12.02.2026

---
+++ 260212_2_Method_ElektrikFaturasi_Cozumhoca/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _260212_2_Method_ElektrikFaturasi_Cozumhoca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EkranaYaz("Harcanan elektrik değerini kw cinsinden giriniz: ");
+            //double kw = OkuDouble();
+            //EkranaYaz("Faturaniz: " + HesaplaFatura(kw));
+            //yukaridaki iki satir kodu tek bir satir ile kodlayabiliriz
+            EkranaYaz("Faturanız: " + HesaplaFatura(OkuDouble()));
+
+        }
+        static void EkranaYaz(string metin)
+        {
+            Console.WriteLine(metin);
+        }
+        static double OkuDouble()
+        {
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        static double HesaplaFatura(double kw)
+        {
+            
+            //double fatura =0;
+            if (kw > 0 && kw <= 150)
+            {
+                return kw * 1.06;
+            }
+            else if (kw > 150 && kw <= 300)
+            {
+                //double ilk150 = 140; //150kw
+                //175kw-> 150kw+25kw
+                //175 kw-> 150kw(140 TL)+25kw(*1.16)
+                return 140 + (kw - 150) * 1.16;
+            }
+            else if (kw > 300 && kw <= 450)
+                return 320 + (kw - 300) * 1.25;
+            else if (kw > 450 && kw <= 650)
+                return 530 + (kw - 450) * 1.29;
+            else
+                return 800 + (kw - 650) * 1.32;
+        }
+         
+
+    }
+}
---
+++ 260212_2_Method_ElektrikFaturasi_Ornek/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _260212_2_Method_ElektrikFaturasi_Ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+ Bir elektrik faturası için bazı şartlar ile fatura hesabı yapılmaktadır. Bu şartlar
+eğer kullanılan elektrik kw olarak 0-150 arasında ise her kw için 1.06 Tl ile çarpılarak hesaplanmaktadır
+--60*1.06=>       
+eğer kullanılan elektrik kw olarak 150-300 arasında ise ilk 150 kw için 140 TL, 150 kw ve üstü içinde her kw için 1.16 Tl ile çarpılarak hesaplanmaktadır
+eğer kullanılan elektrik kw olarak 300-450 arasında ise ilk 300 kw için 320 TL, 300 kw ve üstü içinde her kw için 1.25 Tl ile çarpılarak hesaplanmaktadır
+ eğer kullanılan elektrik kw olarak 450-650 arasında ise ilk 450 kw için 530 TL, 450 kw ve üstü içinde her kw için 1.29 Tl ile çarpılarak hesaplanmaktadır
+eğer 650 ve üstü ise ilk 650 için 800 Tl, 650 ve üstü içnde 1.32 Tl   ile çarpılarak hesaplanmaktadır
+bu şartlar kullanıcının gireceği harcadoğı elektriği kw cinsinden  aldoktan sonra ödeyeceği faturayı hesaplayınız?
+
+
+ */
+            double fatura = 0;
+            FaturaHesapla();
+        }
+        static double FaturaHesapla ()
+        {
+            Console.WriteLine("Lütfen kw giriniz: ");
+            double fatura = 0;
+            double kw = Convert.ToDouble(Console.ReadLine());
+            
+            if (kw > 0 && kw <= 150)
+            {
+                fatura = kw * 1.06;
+                Console.WriteLine("Harcanan:{0} ve fatura:{1} ", kw, fatura);
+
+            }
+            // kullanılan elektrik kw olarak 150-300 arasında ise ilk 150 kw için 140 TL, 150 kw ve üstü içinde her kw için 1.16 Tl ile çarpılarak hesaplanmaktadır
+            else if (kw > 150 && kw <= 300) 
+            {
+                //160 harcanan kw
+                //160-150=>140=>(fatura-150)=>140
+                //10 *1.16=>(fatura-150)*1.16
+                //Ödenecek=140+(fatura-150)*1.16;
+                fatura = (kw - 150) * 1.16;
+                Console.WriteLine("Harcanan:{0} ve fatura:{1} ", kw, fatura);
+            }
+            else if (kw > 300 && kw <= 450)
+            {
+                // 320-300
+                fatura = 320 + (kw - 300) * 1.25;
+                Console.WriteLine("Harcanan:{0} ve fatura:{1} ", kw, fatura);
+            }
+            else if (kw > 450 && kw <= 650)
+            {
+                // 450-530
+                fatura = 530 + (kw - 450) * 1.29;
+                Console.WriteLine("harcanan:{0} ve fatura:{1} ", kw, fatura);
+            }
+            else if(kw>650)
+            {
+                fatura = 850 - (kw - 650) * 1.32;
+                Console.WriteLine("harcanan:{0} ve fatura:{1} ",kw,fatura);
+                
+            }
+            return fatura;
+
+        }
+    }
+}
