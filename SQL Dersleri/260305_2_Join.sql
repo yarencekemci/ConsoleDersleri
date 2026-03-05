d95fbf0c97f1d573f5496db2dda59c9c40660ee9
yarencekemci <yarencekemci@gmail.com>
2026-03-06 00:14:20 +0300

    6.03 SQL

--- SQL Dersleri/260305_2_Join.sql
+++ SQL Dersleri/260305_2_Join.sql
@@ -24,7 +24,49 @@ order by BolumlerID
 
 --ÖDEVLER
 --1)hangi öğrencilerin sınıf ataması olmamıştır
---2)Hangi sınıflara hanüz kayıt yapılamıştır
+select o.OgrencilerID,o.OgrenciAdi,o.OgrenciSoyadi,o.SinifID from ogrenciler as o
+left join Sınıflar as s on o.SinifID=s.SınıflarID
+where s.SınıflarID is null
+
+--2)Hangi sınıflara hanüz kayıt yapılamıştır 
+select
+S.SınıflarID,
+S.SınıfNo,
+S.SınıfSubesi
+from sınıflar as s 
+left join ogrenciler o on o.SinifID=s.SınıflarID
+where o.OgrencilerID is null
+
 --3)her bir öğrencinin aldığı ders sayısı
+select 
+O.OgrencilerID,
+O.OgrenciAdi,
+O.OgrenciSoyadi,
+COUNT(od.DersID) as aldigiDersSayisi
+from ogrenciler as o
+left join OgrenciDers as od on od.OgrenciID = o.OgrencilerID
+group by o.ogrencilerID,o.OgrenciAdi,o.OgrenciSoyadi
+order by aldigiDersSayisi desc
+
 --4)her bir velinin iletişimde olduğu öğrencisinin Sınıf öğretmerni bilgisi veli bilgi , öğrenci bilgisi veren script
+select
+v.VelilerID,
+v.VeliAdi,
+v.VeliSoyadi,
+v.Telefon,
+o.OgrencilerID,
+o.OgrenciAdi,
+o.OgrenciSoyadi,
+og.OgretmenlerID,
+og.OgretmenAdi,
+og.OgretmenSoyadi
+from veliler as v
+inner join OgrenciVeli ov on ov.VeliID=v.VelilerID
+inner join ogrenciler o on o.OgrencilerID=ov.OgrenciID
+left join Sınıflar s on s.SınıflarID=o.SinifID 
+left join Ogretmenler og on og.OgretmenlerID=s.OgretmenID 
+order by v.VelilerID , o.OgrenciSoyadi
+
 --5)en çok izin yapan 10 öğrencilerin bilgisi  ve öğrencilerin veli bilgisi listesini veriniz
+
+
